f0272bafc6eaf0e6a1b958278382d26ec6bf1267
Ben Walsh <ben_walsh@uk.ibm.com>
2018-10-26 15:21:27 +0100

    Update copyright information
    Signed-off-by: Ben Walsh <ben_walsh@uk.ibm.com>

--- common/src/fixpath.c
+++ common/src/fixpath.c
@@ -2,22 +2,9 @@
  * ===========================================================================
  * (c) Copyright IBM Corp. 2018, 2018 All Rights Reserved
  * ===========================================================================
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, see <http://www.gnu.org/licenses/>.
- *
- * ===========================================================================
- *
+ */
+
+/*
  * Copyright (c) 2011, 2012, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
