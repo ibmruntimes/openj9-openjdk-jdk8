db064da063befac72e393d5e188f5562202daf36
Ben Walsh <ben_walsh@uk.ibm.com>
2018-10-31 17:00:58 +0000

    Backport files with copyright containing CPE
    Signed-off-by: Ben Walsh <ben_walsh@uk.ibm.com>

--- jdk/src/macosx/native/sun/font/CCharToGlyphMapper.m
+++ jdk/src/macosx/native/sun/font/CCharToGlyphMapper.m
@@ -4,7 +4,9 @@
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -105,9 +107,10 @@ JNF_COCOA_ENTER(env);
         AllocateGlyphBuffer(env, awtFont, count,
                            (UniChar *)unicodesAsChars, glyphs);
 
-    (*env)->ReleasePrimitiveArrayCritical(env, unicodes,
-                                          unicodesAsChars, JNI_ABORT);
+        (*env)->ReleasePrimitiveArrayCritical(env, unicodes,
+                                              unicodesAsChars, JNI_ABORT);
     }
 
 JNF_COCOA_EXIT(env);
 }
+
