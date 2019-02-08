782bdf245c137e093b84f0d1fe5bd16d17b0c555
Jerry Lui <jerrylui@ibm.com>
2019-02-08 17:39:54 +0000

    Fix and re-enable NativeDigest Fixes segfault in NativeDigest under some conditions and adds more error checking in Native Digest, Native CBC and Native GCM routines.
    Signed-off-by: Jerry Lui <jerrylui@ibm.com>

--- closed/adds/jdk/src/share/classes/com/sun/crypto/provider/NativeGaloisCounterMode.java
+++ closed/adds/jdk/src/share/classes/com/sun/crypto/provider/NativeGaloisCounterMode.java
@@ -455,11 +455,14 @@ final class NativeGaloisCounterMode extends FeedbackCipher {
 
         byte[] aad = ((aadBuffer == null || aadBuffer.size() == 0) ? emptyAAD : aadBuffer.toByteArray());
 
-        nativeCrypto.GCMEncrypt(key, key.length,
-              iv, iv.length,
-              in, inOfs, len,
-              out, outOfs,
-              aad, aad.length, tagLenBytes);
+        int ret = nativeCrypto.GCMEncrypt(key, key.length,
+                iv, iv.length,
+                in, inOfs, len,
+                out, outOfs,
+                aad, aad.length, tagLenBytes);
+        if (ret == -1) {
+            throw new ProviderException("Error in Native GaloisCounterMode");
+        }
 
         return (len + tagLenBytes);
     }
@@ -551,9 +554,12 @@ final class NativeGaloisCounterMode extends FeedbackCipher {
                 out, outOfs,
                 aad, aad.length, tagLenBytes);
 
-        if (ret == -1) {
+        if (ret == -2) {
             throw new AEADBadTagException("Tag mismatch!");
+        } else if (ret == -1) {
+            throw new ProviderException("Error in Native GaloisCounterMode");
         }
+
 
         return ret;
     }
