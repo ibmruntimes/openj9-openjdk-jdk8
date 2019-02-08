afbd8dfaa631833521a07615f5e981513e08b9cd
andrew-m-leonard <andrew_m_leonard@uk.ibm.com>
2019-02-08 15:52:10 +0000

    Exclude _openj9 tag for version search
    Signed-off-by: andrew-m-leonard <andrew_m_leonard@uk.ibm.com>

--- common/autoconf/generated-configure.sh
+++ common/autoconf/generated-configure.sh
@@ -4351,7 +4351,7 @@ VS_SDK_PLATFORM_NAME_2017=
 #CUSTOM_AUTOCONF_INCLUDE
 
 # Do not change or remove the following line, it is needed for consistency checks:
-DATE_WHEN_GENERATED=1549028451
+DATE_WHEN_GENERATED=1549641052
 
 ###############################################################################
 #
