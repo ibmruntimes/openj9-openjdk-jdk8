24f9fc7103cdf8b29a79794d50f2e6bebc4e9ecb
andrew-m-leonard <andrew_m_leonard@uk.ibm.com>
2019-07-17 11:13:57 +0100

    Update get_j9_sources.sh to v0.15.0-release and ext versions
    Signed-off-by: andrew-m-leonard <andrew_m_leonard@uk.ibm.com>

--- common/autoconf/generated-configure.sh
+++ common/autoconf/generated-configure.sh
@@ -4397,7 +4397,7 @@ VS_SDK_PLATFORM_NAME_2017=
 #CUSTOM_AUTOCONF_INCLUDE
 
 # Do not change or remove the following line, it is needed for consistency checks:
-DATE_WHEN_GENERATED=1562248326
+DATE_WHEN_GENERATED=1563358341
 
 ###############################################################################
 #
