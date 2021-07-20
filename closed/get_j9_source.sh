96acb976bc899339788831bfa9d6dcdb5eb2f2c9
Keith W. Campbell <keithc@ca.ibm.com>
2021-07-20 17:48:13 -0400

    Update versions to build the openj9-0.27.0 release
    Signed-off-by: Keith W. Campbell <keithc@ca.ibm.com>

--- closed/get_j9_source.sh
+++ closed/get_j9_source.sh
@@ -50,7 +50,7 @@ fi
 declare -A j9repos
 declare -A branches
 declare -A default_j9repos=( [openj9]=eclipse-openj9/openj9 [omr]=eclipse-openj9/openj9-omr )
-declare -A default_branches=( [openj9]=v0.27.0-release [omr]=v0.27.0-release )
+declare -A default_branches=( [openj9]=openj9-0.27.0 [omr]=openj9-0.27.0 )
 declare -A commands
 declare -A shas
 declare -A references
