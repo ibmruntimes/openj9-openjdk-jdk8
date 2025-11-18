ff5ab5957fbbefa1bf7910192a8777a0cd53ba7d
Gengchen Tuo <gengchen.tuo@ibm.com>
2025-11-18 10:30:06 -0500

    Fix leaked FutureValue in ClassByNameCache
    Clean FutureValue of classes loaded by non-system loaders on two exit
    paths.
    Move canonicalLoaderRefs.put() call out of assert to ensure it occurs
    independently of whether assertion checking is enabled.
    Related: https://github.com/ibmruntimes/openj9-openjdk-jdk/issues/1053
    
    Signed-off-by: Gengchen Tuo <gengchen.tuo@ibm.com>

--- closed/adds/jdk/src/share/classes/java/io/ClassByNameCache.java
+++ closed/adds/jdk/src/share/classes/java/io/ClassByNameCache.java
@@ -1,6 +1,6 @@
 /*
  * ===========================================================================
- * (c) Copyright IBM Corp. 2017, 2022 All Rights Reserved
+ * (c) Copyright IBM Corp. 2017, 2025 All Rights Reserved
  * ===========================================================================
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -73,7 +73,8 @@ final class ClassByNameCache {
 
     private void setCanonicalSystemLoaderRef(ClassLoader loader) {
         LoaderRef newKey = new LoaderRef(loader, staleLoaderRefs, true);
-        assert (canonicalLoaderRefs.put(newKey, newKey) == null);
+        LoaderRef oldRef = canonicalLoaderRefs.put(newKey, newKey);
+        assert oldRef == null;
     }
 
     /*
@@ -116,6 +117,7 @@ final class ClassByNameCache {
         Object resultLoaderObj =
             LoaderRef.getLoaderObj(result.getClassLoader());
         if (getCanonicalLoaderRef(resultLoaderObj).isSystem == false) {
+            cache.remove(key);
             return;
         }
 
@@ -157,8 +159,12 @@ final class ClassByNameCache {
         }
 
         if (value instanceof FutureValue) {
-
-            return ((FutureValue)value).get();
+            try {
+                return ((FutureValue)value).get();
+            } catch (ClassNotFoundException e) {
+                cache.remove(key);
+                throw e;
+            }
         }
 
         return (Class<?>)value;
